0a30204a8f2b2190e0187b5c95dc14a15c1b2b6b
hf <fuhi1983@gmail.com>
2018-09-01 15:00:53 +0900

    create new project

---
+++ FJSAlertControllerExtension/FJSAlertControllerExtension.h
@@ -0,0 +1,19 @@
+//
+//  FJSAlertControllerExtension.h
+//  FJSAlertControllerExtension
+//
+//  Created by hf on 2018/09/01.
+//  Copyright © 2018年 swift-studying.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FJSAlertControllerExtension.
+FOUNDATION_EXPORT double FJSAlertControllerExtensionVersionNumber;
+
+//! Project version string for FJSAlertControllerExtension.
+FOUNDATION_EXPORT const unsigned char FJSAlertControllerExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FJSAlertControllerExtension/PublicHeader.h>
+
+
