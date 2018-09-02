49780da43e88154e961e32a5311c8f32462f23ff
hf <fuhi1983@gmail.com>
2018-09-02 12:16:51 +0900

    update example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -25,22 +25,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func changeBgColor(_ sender: UIBarButtonItem) {
-        let greenAction = UIAlertAction(title: "green", style: .default)
-        let yellowAction = UIAlertAction(title: "yellow", style: .default)
-        let redAction = UIAlertAction(title: "red", style: .default)
-        let cancelAction = UIAlertAction(title: "cancel", style: .cancel)
-        
-        let alertController = UIAlertController(title: "change background color", message: "select color", preferredStyle: .actionSheet, actions: [greenAction,yellowAction,redAction, cancelAction]) { (index, _) in
-            
-            switch index {
-            case 0: self.view.backgroundColor = .green
-            case 1: self.view.backgroundColor = .yellow
-            case 2: self.view.backgroundColor = .red
-            default: break
-            }
-        }
-        
-        present(alertController, animated: true)
+let greenAction = UIAlertAction(title: "green", style: .default)
+let yellowAction = UIAlertAction(title: "yellow", style: .default)
+let redAction = UIAlertAction(title: "red", style: .default)
+let cancelAction = UIAlertAction(title: "cancel", style: .cancel)
+
+let alertController = UIAlertController(title: "change background color", message: "select color", preferredStyle: .actionSheet, actions: [greenAction,yellowAction,redAction, cancelAction]) { (index, _) in
+    
+    switch index {
+    case 0: self.view.backgroundColor = .green
+    case 1: self.view.backgroundColor = .yellow
+    case 2: self.view.backgroundColor = .red
+    default: break
+    }
+}
+
+present(alertController, animated: true)
     }
     
 }
