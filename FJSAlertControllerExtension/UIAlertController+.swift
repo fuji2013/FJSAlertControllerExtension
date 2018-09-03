f3a8a24308834c7ecdbd2020b952e6c8fd791904
hf <fuhi1983@gmail.com>
2018-09-03 16:02:26 +0900

    add AlertButton enum

--- FJSAlertControllerExtension/UIAlertController+.swift
+++ FJSAlertControllerExtension/UIAlertController+.swift
@@ -8,7 +8,55 @@
 
 import UIKit
 
+public class FJSAlertControllerExtension {
+    public enum AlertButton {
+        case cancel(String?)
+        case `default`(String?)
+        case destructive(String?)
+        
+        var title: String? {
+            switch self {
+            case .cancel(let title), .default(let title), .destructive(let title):
+                return title
+            }
+        }
+        
+        var style: UIAlertActionStyle {
+            switch self {
+            case .cancel:
+                return .cancel
+            case .default:
+                return .default
+            case .destructive:
+                return .destructive
+            }
+        }
+        
+        fileprivate func toAlertAction(handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
+            return UIAlertAction(title: title, style: style, handler: handler)
+        }
+    }
+}
+
 public extension UIAlertController {
+    public convenience init(title: String?, message: String?, preferredStyle: UIAlertControllerStyle, buttons: [FJSAlertControllerExtension.AlertButton]) {
+        self.init(title: title, message: message, preferredStyle: preferredStyle)
+        self.addActions(buttons.map { $0.toAlertAction() })
+    }
+    
+    /**
+     return actionHandler with index of selected action and selected action.
+     */
+    public convenience init(title: String?, message: String?, preferredStyle: UIAlertControllerStyle, buttons: [FJSAlertControllerExtension.AlertButton], actionHandler: ((Int, UIAlertAction) -> Void)?) {
+        
+        let handlerRemovedActions = buttons.enumerated().map { i, button in
+            return UIAlertAction(title: button.title, style: button.style) { selectedAction in
+                actionHandler?(i, selectedAction)
+            }
+        }
+        self.init(title: title, message: message, preferredStyle: preferredStyle, actions: handlerRemovedActions)
+    }
+    
     /**
      convenience initializer. can add actions when initialize
     */
--- FJSRxAlertControllerExtension/FJSAlertController+Rx.swift
+++ FJSRxAlertControllerExtension/FJSAlertController+Rx.swift
@@ -10,6 +10,17 @@ import FJSAlertControllerExtension
 import RxSwift
 
 public extension Reactive where Base: UIViewController {
+    public func present(title: String?, message: String?, preferredStyle: UIAlertControllerStyle, buttons: [FJSAlertControllerExtension.AlertButton], animated: Bool, completion: (() -> Void)? = nil) -> Observable<(Int, UIAlertAction)> {
+        return Observable.create({ (observer) -> Disposable in
+            let newAlertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle, buttons: buttons) { index, selectedAction in
+                observer.onNext((index, selectedAction))
+                observer.onCompleted()
+            }
+            self.base.present(newAlertController, animated: animated, completion: completion)
+            return Disposables.create()
+        })
+    }
+    
     public func present(title: String?, message: String?, preferredStyle: UIAlertControllerStyle, actions: [UIAlertAction], animated: Bool, completion: (() -> Void)? = nil) -> Observable<(Int, UIAlertAction)> {
         return Observable.create({ (observer) -> Disposable in
             let newAlertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle, actions: actions) { index, selectedAction in
